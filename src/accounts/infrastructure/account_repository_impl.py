8fd80cea9a53a0a4d71959758b04ee205fc1fd4a
Потапов Андрей Иванович <AIPotapov@alfabank.ru>
2026-03-31 12:07:32 +0300

    Revert "excess containers bug fix"
    This reverts commit d7586e64

--- src/accounts/infrastructure/account_repository_impl.py
+++ src/accounts/infrastructure/account_repository_impl.py
@@ -6,6 +6,7 @@ from pymongo import AsyncMongoClient
 from src.accounts.domain.interfaces.account_repository import IAccountRepository
 from src.accounts.domain.models.account import Account
 from src.accounts.domain.models.payment import Payment
+from src.accounts.domain.models.person import Person
 
 
 class AccountRepository(IAccountRepository):
@@ -13,11 +14,33 @@ class AccountRepository(IAccountRepository):
         self.db = client[db_name]
         self.collection= self.db["accounts"]
 
+    async def get_by_id(self, account_id: str) ->Account:
+        doc = await self.collection.find_one({"id": account_id})
+        if not doc:
+            return None
+
+        person_doc = doc.get("person")
+        person = Person(
+            id=str(person_doc["id"]),
+            name=person_doc["name"])
+        # Преобразуем список платежей
+        payments = []
+        for p in doc.get("payments", []):
+            payments.append(Payment(
+                id=str(p["_id"]),
+                sum=p["sum"]))
+        # Собираем Account
+        return Account(
+            id=str(doc["id"]),
+            name=doc["name"],
+            person=person,
+            payments=payments )
+
     async def create(self, account: Account)->None:
         doc = {
             "id": account.id,
             "name": account.name,
-            "person": { "name": account.person.name},
+            "person": {"id": account.person.id, "name": account.person.name},
             "payments": [{"id": pay.id, "purpose":pay.purpose, "sum": str(pay.sum)} for pay in account.payments]
         }
         await self.collection.insert_one(doc)
